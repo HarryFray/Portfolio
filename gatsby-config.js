d94c4fdcf266030bdd55904c6414f6d2eb76e57e
HarryFray <harry.fray7@gmail.com>
2020-02-20 22:01:07 -0600

    feat(site): configuring gatsby headless cms

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,6 +19,7 @@ module.exports = {
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     `gatsby-plugin-emotion`,
+    `gatsby-plugin-netlify-cms`,
     {
       resolve: `gatsby-plugin-google-fonts`,
       options: {
